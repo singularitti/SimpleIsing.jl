5d03357cccd813b81194035e60c11113bdbc3bbf
singularitti <singularitti@outlook.com>
2022-11-15 07:37:42 -0500

    Add types `Lattice` & `Evolution` in src/SimpleIsing.jl

--- src/SimpleIsing.jl
+++ src/SimpleIsing.jl
@@ -1,5 +1,16 @@
 module SimpleIsing
 
-# Write your package code here.
+using StaticArrays: MMatrix, MArray
+
+export Lattice, Evolution
+
+struct Lattice{S1,S2}
+    spins::MMatrix{S1,S2,Bool}
+end
+Lattice(spins::AbstractMatrix{Bool}) = Lattice{size(spins, 1),size(spins, 2)}(spins)
+
+struct Evolution{S1,S2,T}
+    history::MArray{Tuple{S1,S2},Bool,3}
+end
 
 end
