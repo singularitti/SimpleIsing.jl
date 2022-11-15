faca75ba6636a64f77c2ddf247becedd366a598b
singularitti <singularitti@outlook.com>
2022-11-15 07:37:56 -0500

    Add `show` for `Lattice`

--- src/SimpleIsing.jl
+++ src/SimpleIsing.jl
@@ -13,4 +13,15 @@ struct Evolution{S1,S2,T}
     history::MArray{Tuple{S1,S2},Bool,3}
 end
 
+function Base.show(io::IO, lattice::Lattice)
+    if get(io, :compact, false) || get(io, :typeinfo, nothing) == typeof(lattice)
+        Base.show_default(IOContext(io, :limit => true), lattice)  # From https://github.com/mauro3/Parameters.jl/blob/ecbf8df/src/Parameters.jl#L556
+    else
+        println(io, string(typeof(lattice)))
+        for row in eachrow(lattice.spins)
+            println(io, ' ', join(Int.(row), ' '^2))
+        end
+    end
+end
+
 end
