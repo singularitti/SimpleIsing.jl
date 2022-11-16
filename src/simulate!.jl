722606d991c6908bf1c9a585fa8e944e9fc80701
singularitti <singularitti@outlook.com>
2022-11-15 22:39:08 -0500

    Fix `simulate!` for `eᵢ_new <= eᵢ_old`

--- src/simulate!.jl
+++ src/simulate!.jl
@@ -9,13 +9,15 @@ function simulate!(lattice::Lattice, β, J, B, ::Plain)
         trial_spin = -lattice[index]  # Trial move, no in-place update now!
         eᵢ_old = energy(lattice, index, J, B)
         eᵢ_new = energy(sum(neighborspins(lattice, index)), trial_spin, J, B)
+        P = exp(-β * (eᵢ_new - eᵢ_old))
         if eᵢ_new > eᵢ_old  # Moving into a higher-energy state with probability P < 1
-            P = exp(-β * (eᵢ_new - eᵢ_old))
             if P > rand()
                 lattice[index] = trial_spin  # Accept the trial move
             end
-        else
-            lattice[index] = trial_spin  # Accept the trial move
+        else  # eᵢ_new <= eᵢ_old
+            if P > rand()  # Always true
+                lattice[index] = trial_spin  # Accept the trial move
+            end
         end
     end
     return lattice
