c06258f5b414fc2d4756e556d57b12d6c9ee5845
singularitti <singularitti@outlook.com>
2022-11-16 00:48:35 -0500

    Change field type of `Evolution` & its `IndexStyle`

--- src/types.jl
+++ src/types.jl
@@ -11,12 +11,9 @@ struct Lattice{T} <: AbstractMatrix{T}
 end
 Lattice(spins::AbstractMatrix) = Lattice{eltype(spins)}(collect(spins))
 
-struct Evolution{T} <: AbstractArray{T,3}
-    history::Array{T,3}
+struct Evolution{T} <: AbstractVector{Lattice{T}}
+    history::Vector{Lattice{T}}
 end
-# See https://discourse.julialang.org/t/turn-vector-of-matrices-into-3d-array/69777/6
-Evolution(history::AbstractVector{<:Lattice}) =
-    Evolution(reduce((x, y) -> cat(x, y; dims=3), history))
 
 Base.parent(lattice::Lattice) = lattice.spins
 Base.parent(evolution::Evolution) = evolution.history
@@ -24,8 +21,10 @@ Base.parent(evolution::Evolution) = evolution.history
 Base.size(lattice::Lattice) = size(parent(lattice))
 Base.size(evolution::Evolution) = size(parent(evolution))
 
+Base.IndexStyle(::Type{<:Evolution}) = IndexLinear()
+
 Base.getindex(lattice::Lattice, I...) = getindex(parent(lattice), I...)
-Base.getindex(evolution::Evolution, I...) = getindex(parent(evolution), I...)
+Base.getindex(evolution::Evolution, I) = getindex(parent(evolution), I)
 
 Base.setindex!(lattice::Lattice, v, I...) = setindex!(parent(lattice), v, I...)
-Base.setindex!(evolution::Evolution, v, I...) = setindex!(parent(evolution), v, I...)
+Base.setindex!(evolution::Evolution, v, I) = setindex!(parent(evolution), v, I)
