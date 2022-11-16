9474ff03171c3061c2c6749ede5fc46ada3936d8
singularitti <singularitti@outlook.com>
2022-11-15 22:28:42 -0500

    Implement `simulate!` in src/simulate!.jl

--- src/SimpleIsing.jl
+++ src/SimpleIsing.jl
@@ -2,5 +2,6 @@ module SimpleIsing
 
 include("types.jl")
 include("statmech.jl")
+include("simulate!.jl")
 
 end
---
+++ src/simulate!.jl
@@ -0,0 +1,22 @@
+export Plain, simulate!
+
+abstract type Algorithm end
+struct Plain <: Algorithm end
+struct SwendsenWang <: Algorithm end
+
+function simulate!(lattice::Lattice, β, J, B, ::Plain)
+    for index in eachindex(lattice)
+        trial_spin = -lattice[index]  # Trial move, no in-place update now!
+        eᵢ_old = energy(lattice, index, J, B)
+        eᵢ_new = energy(sum(neighborspins(lattice, index)), trial_spin, J, B)
+        if eᵢ_new > eᵢ_old  # Moving into a higher-energy state with probability P < 1
+            P = exp(-β * (eᵢ_new - eᵢ_old))
+            if P > rand()
+                lattice[index] = trial_spin  # Accept the trial move
+            end
+        else
+            lattice[index] = trial_spin  # Accept the trial move
+        end
+    end
+    return lattice
+end
