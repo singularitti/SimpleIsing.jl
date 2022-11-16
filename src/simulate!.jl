9d8a981309e99fefaadb94776df56a4c38c03b9d
singularitti <singularitti@outlook.com>
2022-11-15 22:42:42 -0500

    Merge the two ways in `simulate!`

--- src/simulate!.jl
+++ src/simulate!.jl
@@ -10,14 +10,10 @@ function simulate!(lattice::Lattice, β, J, B, ::Plain)
         eᵢ_old = energy(lattice, index, J, B)
         eᵢ_new = energy(sum(neighborspins(lattice, index)), trial_spin, J, B)
         P = exp(-β * (eᵢ_new - eᵢ_old))
-        if eᵢ_new > eᵢ_old  # Moving into a higher-energy state with probability P < 1
-            if P > rand()
-                lattice[index] = trial_spin  # Accept the trial move
-            end
-        else  # eᵢ_new <= eᵢ_old
-            if P > rand()  # Always true
-                lattice[index] = trial_spin  # Accept the trial move
-            end
+        # No matter whether you move into a higher-energy state with probability P < 1,
+        # or move into a lower-energy state with probability 1, always do:
+        if P > rand()
+            lattice[index] = trial_spin  # Accept the trial move
         end
     end
     return lattice
