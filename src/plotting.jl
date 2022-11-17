4e71d0394b7efd0e7dcc66a784d7e621649d3e6a
singularitti <singularitti@outlook.com>
2022-11-17 02:21:28 -0500

    Update src/plotting.jl args

--- src/plotting.jl
+++ src/plotting.jl
@@ -1,9 +1,12 @@
 using RecipesBase
 
 @recipe function f(lattice::Lattice)
+    yflip --> true  # Set the origin to the upper left corner, see https://github.com/MakieOrg/Makie.jl/issues/46
     xlims --> extrema(axes(lattice, 1))
     ylims --> extrema(axes(lattice, 2))
     xguide --> "x"
+    xguideposition --> :top  # Place xguide along top axis
+    xmirror --> true  # Place xticks along top axis, see https://github.com/JuliaPlots/Plots.jl/issues/337
     yguide --> "y"
     guidefontsize --> 8
     tickfontsize --> 6
