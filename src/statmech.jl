6bd83de2987accee2ca091337321098f1ed84dc6
singularitti <singularitti@outlook.com>
2022-11-18 22:48:21 -0500

    Rewrite `isneighbor` as a type `IsNeighbor`

--- src/statmech.jl
+++ src/statmech.jl
@@ -1,16 +1,25 @@
 using Statistics: mean
 
-export findneighbors, neighborspins, energy, partition_function, magnetization
+export isneighbor, neighborspins, energy, partition_function, magnetization
 
-function Base.findall(::typeof(isneighbor), lattice::Lattice, index::CartesianIndex)
+struct IsNeighbor
+    index::CartesianIndex
+end
+function (I::IsNeighbor)(J::CartesianIndex)
+    δᵢ, δⱼ = Tuple(I - J)
+    return 0 <= δᵢ^2 + δⱼ^2 <= 1
+end
+isneighbor(I::CartesianIndex, J::CartesianIndex) = IsNeighbor(I)(J)
+
+function Base.findall(::typeof(IsNeighbor), lattice::Lattice, index::CartesianIndex)
     width, height = size(lattice)
     i, j = Tuple(index)  # See https://discourse.julialang.org/t/unpacking-cartesianindex/27374/6
     return CartesianIndex(mod1(i + 1, width), j),
     CartesianIndex(mod1(i - 1, width), j), CartesianIndex(i, mod1(j + 1, height)),
     CartesianIndex(i, mod1(j - 1, height))
 end
-Base.findall(::typeof(isneighbor), lattice::Lattice, i, j) =
-    findall(isneighbor, lattice, CartesianIndex(i, j))
+Base.findall(f::typeof(IsNeighbor), lattice::Lattice, i, j) =
+    findall(f, lattice, CartesianIndex(i, j))
 Base.findall(::typeof(isneighbor), lattice::Lattice) =
     map(Base.Fix1(circshift, lattice), ((1, 0), (-1, 0), (0, 1), (0, -1)))
 
