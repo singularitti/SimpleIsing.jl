f3e62dfe642f18779ac9aad3b92764122667b009
singularitti <singularitti@outlook.com>
2022-11-19 00:53:32 -0500

    Recover `findneighbors`

--- src/statmech.jl
+++ src/statmech.jl
@@ -1,30 +1,18 @@
 using Statistics: mean
 
-export isneighbor, neighborspins, energy, partition_function, magnetization
+export findneighbors, neighborspins, energy, partition_function, magnetization
 
-struct IsNeighbor
-    index::CartesianIndex
-end
-function (I::IsNeighbor)(J::CartesianIndex)
-    δᵢ, δⱼ = Tuple(I - J)
-    return 0 <= δᵢ^2 + δⱼ^2 <= 1
-end
-isneighbor(I::CartesianIndex, J::CartesianIndex) = IsNeighbor(I)(J)
-
-function Base.findall(::typeof(IsNeighbor), lattice::Lattice, index::CartesianIndex)
+function findneighbors(lattice::Lattice, index::CartesianIndex)
     width, height = size(lattice)
     i, j = Tuple(index)  # See https://discourse.julialang.org/t/unpacking-cartesianindex/27374/6
     return CartesianIndex(mod1(i + 1, width), j),
     CartesianIndex(mod1(i - 1, width), j), CartesianIndex(i, mod1(j + 1, height)),
     CartesianIndex(i, mod1(j - 1, height))
 end
-Base.findall(f::typeof(IsNeighbor), lattice::Lattice, i, j) =
-    findall(f, lattice, CartesianIndex(i, j))
-Base.findall(::typeof(isneighbor), lattice::Lattice) =
-    map(Base.Fix1(circshift, lattice), ((1, 0), (-1, 0), (0, 1), (0, -1)))
+findneighbors(lattice::Lattice, i, j) = findneighbors(lattice, CartesianIndex(i, j))
 
 function neighborspins(lattice::Lattice, index::CartesianIndex)
-    neighbors = findall(isneighbor, lattice, index)
+    neighbors = findneighbors(lattice, index)
     return map(Base.Fix1(getindex, lattice), neighbors)
 end
 neighborspins(lattice::Lattice, i, j) = neighborspins(lattice, CartesianIndex(i, j))
