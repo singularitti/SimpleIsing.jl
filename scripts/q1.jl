ce85a0cf5769f076b076fbad15836eedfcff4e42
singularitti <singularitti@outlook.com>
2022-11-20 17:35:16 -0500

    Update scripts/q1.jl for more J values

--- scripts/q1.jl
+++ scripts/q1.jl
@@ -5,8 +5,16 @@ using Statistics
 N = 64  # Size of the lattice
 lattice = Lattice(ones(N, N))
 β = 1
-J = 0.435
-nsteps = 8000
-nsteps_thermal = 2000  # Number of steps needed to be thermalized
-trace = simulate!(lattice, nsteps, β, J, 0, SwendsenWang())
-fit(lattice, trace[nsteps_thermal:end], [0.2588312476412214, 32.53734520801327])
+# J = 0.425
+# nsteps = 8000
+# nsteps_thermal = 2000  # Number of steps needed to be thermalized
+# trace = simulate!(lattice, nsteps, β, J, 0, SwendsenWang())
+# fit(lattice, trace[nsteps_thermal:end], [0.2588312476412214, 32.53734520801327])
+𝐉 = [0.425, 0.43, 0.435, 0.438, 0.439, 0.44, 0.4405, 0.4406868, 0.4407, 0.441, 0.442]
+params = map(𝐉) do J
+    nsteps = 8000
+    nsteps_thermal = 2000  # Number of steps needed to be thermalized
+    trace = simulate!(lattice, nsteps, β, J, 0, SwendsenWang())
+    fit(lattice, trace[nsteps_thermal:end], [0.2588312476412214, 32.53734520801327]).param
+end
+paramplot(𝐉, [param[2] for param in params])
