18783ef744e6c59db13133e98879c3a6347fa7e8
singularitti <singularitti@outlook.com>
2022-11-21 23:12:36 -0500

    Rewrite scripts/problem.jl

--- scripts/problem.jl
+++ scripts/problem.jl
@@ -3,7 +3,8 @@ push!(LOAD_PATH, dirname(pwd()))
 using LaTeXFigures: Figure, latexformat
 using LsqFit: curve_fit
 using Plots: plot, savefig
-using SimpleIsing: Lattice, SwendsenWang, Modeller, simulate!, spincor, paramplot, corplot!
+using ProgressMeter: @showprogress
+using SimpleIsing: Lattice, SwendsenWang, Modeller, simulate!, spincor, paramplot!, corplot!
 using Statistics: mean
 
 plotsdir() = abspath("../tex/plots")
@@ -11,47 +12,46 @@ if !isdir(plotsdir())
     mkpath(plotsdir())
 end
 
-β = 1
-nsteps = 5000
-nsteps_thermal = 2000  # Number of steps needed to be thermalized
-𝐉 = [
-    0.425,
-    0.43,
-    0.435,
-    0.438,
-    0.439,
-    0.44,
-    0.4405,
-    0.4406868,
-    0.4407,
-    0.441,
-    0.442,
-    0.45,
-    0.5,
-]
-N = 32  # Size of the lattice
-lattice = Lattice(ones(N, N))
-𝐳 = 1:N
-𝐚 = Float64[]
-𝐛 = Float64[]
-𝚺z = []
+const β = 1
+const nsteps = 5000
+const nsteps_thermal = 2000  # Number of steps needed to be thermalized
+𝐉 = [0.44, 0.439, 0.438, 0.435, 0.43, 0.425, 0.4]  # Increasing temperature
+boxsizes = [32, 64, 128, 256]  # Size of the lattice
+A = Matrix{Float64}(undef, length(𝐉), length(boxsizes))  # Parameter a for each J for each N
+B = Matrix{Float64}(undef, length(𝐉), length(boxsizes))  # Parameter b for each J for each N
 
-for J in 𝐉
-    trace = simulate!(lattice, nsteps, β, J, 0, SwendsenWang())
-    Σz = map(spincor(trace), 𝐳)
-    push!(𝚺z, Σz)
-    𝚺̄z = map(mean, Σz)
-    a, b = curve_fit(Modeller(N), 𝐳, 𝚺̄z, [0.2588, 32.537]).param
-    push!(𝐚, a)
-    push!(𝐛, b)
+function plot_bJ(𝐉, 𝐛, N)
+    plot()  # Start a new figure
+    paramplot!(𝐉, 𝐛)
+    figname = string("b(J)_N=", N, ".pdf")
+    savefig(joinpath(plotsdir(), figname))
+    return clipboard(latexformat(Figure(figname; caption=raw"", label="fig:bJ", width=0.8)))
 end
-paramplot(𝐉, 𝐛)
-clipboard(latexformat(Figure("J-b.pdf"; caption="", label="", width=1)))
-savefig(joinpath(plotsdir(), "J-b.pdf"))
 
-plot()
-for (a, b, J) in zip(𝐚, 𝐛, 𝐉)
-    corplot!(𝐳, Modeller(lattice)(𝐳, [a, b]); label=raw"$J = " * string(J) * raw" $")
+function plot_correlation(𝐚, 𝐛, 𝐉, N)
+    plot()  # Start a new figure
+    𝐳 = 1:N  # Each z
+    return map(𝐚, 𝐛, 𝐉) do a, b, J
+        corplot!(𝐳, Modeller(N)(𝐳, [a, b]); label=string(raw"$J = ", J, raw" $"))
+        figname = string("correlation_N=", N, ".pdf")
+        savefig(joinpath(plotsdir(), figname))
+        clipboard(latexformat(Figure(figname; caption=raw"", label="fig:corr", width=0.8)))
+    end
 end
-savefig(joinpath(plotsdir(), "correlation.pdf"))
-clipboard(latexformat(Figure("correlation.pdf"; caption="", label="", width=1)))
+
+for (i, N) in enumerate(boxsizes)
+    lattice = Lattice(ones(N, N))
+    𝐳 = 1:N  # Each z
+    𝚺z = []
+    @showprogress for (j, J) in enumerate(𝐉)
+        trace = simulate!(lattice, nsteps, β, J, 0, SwendsenWang())
+        Σz = map(spincor(trace), 𝐳)  # Vector of vectors, Σ(z) for each z for each timestep for this J for this N
+        push!(𝚺z, Σz)
+        𝚺̄z = map(mean, Σz)  # Vector, ensemble average ⟨Σ(z)⟩ for each z for this J for this N
+        a, b = curve_fit(Modeller(N), 𝐳, 𝚺̄z, [0.2588, 32.537]).param  # Parameters for ⟨Σ(z)⟩
+        A[j, i] = a
+        B[j, i] = b
+    end
+    plot_bJ(𝐉, B[:, i], N)
+    plot_correlation(A[:, i], B[:, i], 𝐉, N)
+end
