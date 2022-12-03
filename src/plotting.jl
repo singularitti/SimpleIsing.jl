7fc4c0765da1449cd058253dc26007badd6f0740
singularitti <singularitti@outlook.com>
2022-12-03 14:31:42 -0500

    Add figure `size` in `heatmap` for lattices

--- src/plotting.jl
+++ src/plotting.jl
@@ -3,6 +3,7 @@ using RecipesBase
 export magplot, paramplot, corplot
 
 @recipe function f(lattice::Lattice)
+    size --> (600, 600)
     yflip --> true  # Set the origin to the upper left corner, see https://github.com/MakieOrg/Makie.jl/issues/46
     xlims --> extrema(axes(lattice, 1)) .+ (-0.5, 0.5)  # See https://discourse.julialang.org/t/can-plots-jl-heatmap-coordinates-start-at-1-instead-of-0-5/90385/3
     ylims --> extrema(axes(lattice, 2)) .+ (-0.5, 0.5)
